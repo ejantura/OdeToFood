24d71119c26734fa07d1b4a9d231cd29c2fa5b49
ejantura <ejantura@gmail.com>
2018-03-15 12:04:27 +0100

    Add connection to database

---
+++ OdeToFood/Migrations/201803150747448_InitialCreate.cs
@@ -0,0 +1,44 @@
+namespace OdeToFood.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Restaurants",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        City = c.String(),
+                        Country = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ResteurantReviews",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Rating = c.Int(nullable: false),
+                        Body = c.String(),
+                        RestaurantId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Restaurants", t => t.RestaurantId, cascadeDelete: true)
+                .Index(t => t.RestaurantId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ResteurantReviews", "RestaurantId", "dbo.Restaurants");
+            DropIndex("dbo.ResteurantReviews", new[] { "RestaurantId" });
+            DropTable("dbo.ResteurantReviews");
+            DropTable("dbo.Restaurants");
+        }
+    }
+}
---
+++ OdeToFood/Models/OdeToFoodDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace OdeToFood.Models
+{
+    public class OdeToFoodDb : DbContext
+
+    {
+        public OdeToFoodDb() :  base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Restaurant> Resteurants { get; set; }
+        public DbSet<ResteurantReview> Reviews { get; set; }
+
+    }
+
+}
